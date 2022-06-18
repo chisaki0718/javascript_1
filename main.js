47363658551dd9ae71cf64831207de9b542a5edd
chisaki0718 <jobtech.nagamine@gmail.com>
2022-06-18 18:15:37 +0900

    JavaScript課題①-1修正いたしました

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
+const fizz = 3 ;
+const buzz = 5 ;
+
 for ( let i = 1; i <= 99; i ++ ){
-  if (i % 3 === 0 && i % 5 === 0){
-    console.log('FizzBuzz '+ i );
-  } else if (i % 3 === 0) {
-    console.log('Fizz ' + i );
-  } else if (i % 5 === 0) {
-    console.log('Buzz ' + i );
+  if (i % fizz === 0 && i % buzz === 0){
+    console.log( `FizzBuzz ${i}` );
+  } else if (i % fizz === 0) {
+    console.log(`Fizz ${i}` );
+  } else if (i % buzz === 0) {
+    console.log(`Buzz ${i}` );
   }
 }
